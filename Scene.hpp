a55a1c423feb2a15fd375c80e0f9050db091a195
Vivien <bonnet.vivien@gmail.com>
2023-08-01 22:24:06 +0200

    Removal of a useless interface IScene.

--- Scene.hpp
+++ Scene.hpp
@@ -7,15 +7,8 @@
 #include "SpotLight.hpp"
 #include "span.hpp"
 
-struct IScene
-{
-    constexpr virtual const Camera* GetCamera() const = 0;
-    constexpr virtual span<const Object*> Objects() const = 0;
-    constexpr virtual span<const SpotLight*> Lights() const = 0;
-};
-
 template <std::size_t objectN, std::size_t lightN>
-struct Scene final : public IScene
+struct Scene
 {
     constexpr Scene(const Camera* camera, const std::array<const Object*, objectN>& objects,
                     const std::array<const SpotLight*, lightN>& lights)
@@ -25,15 +18,9 @@ struct Scene final : public IScene
     {
     }
 
-    constexpr const Camera* GetCamera() const override { return m_camera; }
-    constexpr span<const Object*> Objects() const override
-    {
-        return span<const Object*>(m_objects.data(), m_objects.size());
-    }
-    constexpr span<const SpotLight*> Lights() const override
-    {
-        return span<const SpotLight*>(m_lights.data(), m_lights.size());
-    }
+    constexpr const Camera* GetCamera() const { return m_camera; }
+    constexpr span<const Object*> Objects() const { return span<const Object*>(m_objects.data(), m_objects.size()); }
+    constexpr span<const SpotLight*> Lights() const { return span<const SpotLight*>(m_lights.data(), m_lights.size()); }
 
 private:
     const Camera* m_camera;
