8a0c248c2d3c761d2fc16c3b412734ffacef5fc5
Vivien <bonnet.vivien@gmail.com>
2024-02-11 15:15:18 +0100

    convert.sh can convert any file format by first converting to a specific bmp format and then to a C-include file format, using a temporary file.

--- convert.sh
+++ convert.sh
@@ -8,6 +8,9 @@ fi
 src=$1
 dest=$2
 name=$(basename $dest | cut -d . -f 1) # file name without path and without extension.
+temp_file="/tmp/$name.bmp"
 
-file_data=$(cat $src | xxd -i)
+convert -alpha on $src $temp_file
+file_data=$(cat $temp_file | xxd -i)
 echo "constexpr unsigned char $name[] = {\n$file_data\n};" > $dest
+rm $temp_file
