07d8a6a5b84bb3c66a0f7c83afa1bb376d522260
Vivien <bonnet.vivien@gmail.com>
2025-07-06 07:24:08 +0200

    output file is created then renamed in order to let viewer refresh.

--- blink.sh
+++ blink.sh
@@ -10,5 +10,5 @@ echo "Compiling..."
 if clang++ -std=c++20 -fconstexpr-steps=0x4000000 -Wall -Werror -I srcs/ -DSCENE_FILE=\"../scenes/$scene_file\" srcs/main.cpp -o blink
 then
     echo "Running..."
-    ./blink > output.bmp
+    ./blink > tmp.bmp && mv tmp.bmp output.bmp || rm tmp.bmp
 fi
