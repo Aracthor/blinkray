ca2e2b460fe7a7545cdc92e06c02b0b4dbf92fc1
Vivien <bonnet.vivien@gmail.com>
2025-07-07 22:27:20 +0200

    Don't specify file extension to blink.sh.

--- blink.sh
+++ blink.sh
@@ -3,11 +3,11 @@
 scene_file=$1
 if [ -z $scene_file ]
 then
-    scene_file="scene_test.ipp"
+    scene_file="scene_test"
 fi
 
 echo "Compiling..."
-if clang++ -std=c++20 -fconstexpr-steps=0x4000000 -Wall -Werror -I srcs/ -DSCENE_FILE=\"../scenes/$scene_file\" srcs/main.cpp -o blink
+if clang++ -std=c++20 -fconstexpr-steps=0x4000000 -Wall -Werror -I srcs/ -DSCENE_FILE=\"../scenes/$scene_file.ipp\" srcs/main.cpp -o blink
 then
     echo "Running..."
     ./blink > tmp.bmp && mv tmp.bmp output.bmp || rm tmp.bmp
